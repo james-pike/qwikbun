ed34a24b66febfce196a8b4327d497d36347295d
James Pike <jamesandrewpike@gmail.com>
2024-11-19 12:16:39 -0500

    commit

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,10 @@ import tsconfigPaths from "vite-tsconfig-paths";
 export default defineConfig(() => {
   return {
     build: {
+      base: '/',  // Adjust base path for deployment
+      rollupOptions: {
+        input: 'index.html',  // Ensure the correct entry point for your build
+      },
       target: "es2020",
       lib: {
         entry: "./src/index.ts",
